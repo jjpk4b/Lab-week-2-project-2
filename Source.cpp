9f7aabc64f5927b35dcd9215eb8caf99a97a492a
Joseph Parsa <70489478+jjpk4b@users.noreply.github.com>
2020-09-06 12:41:53 -0500

    jj

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int i;
+	cout << "please enter your name: ";
+	cin >> i;
+	int c;
+	cout << "please enter the homework score: ";
+	cin >> c;
+	int b;
+	cout << "Please enter the test score:";
+	cin >> b;
+	int a;
+	cout << "Please enter the quiz score";
+	cin >> a;
+	int d;
+	cout << "Please enter attenance score";
+	cin >> d;
+
+	
+
+	cout << "Grades for " << i << endl;
+	cout << " Homework  : " << c << endl;
+	cout << " Test		:" << b << endl;
+	cout << " Quiz		:" << a << endl;
+	cout << "Attendance :" << d << endl;
+
+
+
+}
